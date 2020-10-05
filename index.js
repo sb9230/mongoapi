04362d2ff984a43a2e61fc50e7bf8df1d5b1e609
sb9230 <sb9230@naver.com>
2020-10-05 12:20:06 +0900

    mongoose step 1

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 var express = require("express");
-
+var mongoose = require("mongoose");
 var app = express();
+
+app.use(express.urlencoded({ extended: false }));
+
+mongoose.connect(
+  "mongodb+srv://root:1234@cluster0.acbo1.mongodb.net/mydb?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 app.get("/", function (request, response) {
   console.log(request);
-  response.send("Hello world");
+  response.send("Hello world!!");
 });
 var port = 8080;
 app.listen(port, function () {
