1d087ed0c2bc969088acca47113493a0b885b30d
sb9230 <sb9230@naver.com>
2020-10-06 15:50:59 +0900

    serverConnect

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ var mongoose = require("mongoose");
 require("dotenv").config();
 var apiRouter = require("./routers/routes");
 var path = require("path");
+var cors = require("cors");
 var app = express();
 var bodyParser = require("body-parser");
 
 app.set("views", path.resolve(__dirname + "/views"));
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api", apiRouter);
 
@@ -17,6 +19,14 @@ mongoose.connect(mongo_url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+var db = mongoose.connection;
+if (!db) {
+  console.log("Error connecting db");
+} else {
+  console.log("db connected success");
+}
+
 app.get("/", function (request, response) {
   console.log(request);
 });
--- routers/routes.js
+++ routers/routes.js
@@ -1,14 +1,89 @@
 var express = require("express");
+const { listIndexes } = require("../models/contactModel");
 var router = express.Router();
 var User = require("../models/contactModel");
+var List = require("../models/listModel");
 
 router.get("/", function (req, res) {
-  //   res.send("hello world!");
+  res.send(`hello world!`);
   res.render("index", { name: "김승빈" });
 });
 
-router.post("/signup", function (req, res) {
-  console.log(req.body[1].name);
-  res.send("Success");
+router.post("/signup", function (req, res, next) {
+  // console.log(req.body);
+  User.findOne({ email: req.body.email }, function (err, user) {
+    if (err) {
+      return next(err);
+    } else if (user) {
+      return res.send("email is already exist!!");
+    } else {
+      var contact = new User();
+      contact.name = req.body.name;
+      contact.password = req.body.password;
+      contact.email = req.body.email;
+      contact.gender = req.body.gender;
+      contact.phone = req.body.phone;
+
+      contact.save(function (err) {
+        if (err) {
+          res.json(err);
+        } else {
+          res.json({ message: "new contact create data", data: contact });
+        }
+      });
+    }
+  });
+  /* req.body에서 .name 을 하면 이름을, .phone을 하면 폰 번호를 불러온다. req.body로만 되어있으면 body에 있는 값 전부를 받아온다.*/
+});
+
+router.post("/login", function (req, res, next) {
+  var email = req.body.id;
+  var password = req.body.password;
+  /*findOne에서 첫번째 email은 스키마의 email, 두번째 email은 router.post("/login")에서 명해준 var email의 email */
+  User.findOne({ email: email }, function (err, user) {
+    if (err) return next(err);
+    else if (!user) return res.send("user not founded");
+    else {
+      if (user.password != password) {
+        res.send("password is invalid");
+      } else {
+        console.log(user);
+        res.send(`welcome to my world! ${user.name}!!!!`);
+      }
+    }
+  });
+});
+
+router.get("/list", function (req, res, next) {
+  res.render("input");
+});
+
+router.post("/list", (req, res, next) => {
+  // console.log(req.body.author);
+  // res.send("success");
+  // var board = new List();
+  var board = new List();
+  board.title = req.body.title;
+  board.contents = req.body.contents;
+  board.author = req.body.author;
+
+  board.save(function (err) {
+    if (err) {
+      return next(err);
+    } else {
+      return res.json("save success!!");
+    }
+  });
+  console.log(req.body);
+});
+
+router.get("/contents", (req, res, next) => {
+  List.find((err, data) => {
+    if (err) {
+      return next(err);
+    } else {
+      return res.json(data);
+    }
+  });
 });
 module.exports = router;
